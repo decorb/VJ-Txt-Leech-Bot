d09a60341c1be6dd89d673f4505c104b84153a9a
decorb <decorb18@gmail.com>
2025-02-24 10:04:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -167,8 +167,12 @@ async def upload(bot: Client, m: Message):
             try:  
                 
                 cc = f'**[📽️] Vid_ID:** {str(count).zfill(3)}.** {𝗻𝗮𝗺𝗲𝟭}{MR}.mkv\n**📚 BATCH NAME :●➠** » **{raw_text0}**\n\n 📥EXTRACTED BY :{MR}'
+
+                
                                                                                                                                    
-                cc1 = f'**[📁] Pdf_ID:** {str(count).zfill(3)}. {𝗻𝗮𝗺𝗲𝟭}{MR}.pdf \n**📚 BATCH NAME :●➠** » **{raw_text0}**\n\ 📥EXTRACTED BY :{MR}' 
+                cc1 = f'**[📁] Pdf_ID:** {str(count).zfill(3)}. {𝗻𝗮𝗺𝗲𝟭}{MR}.pdf \n**📚 BATCH NAME :●➠** » **{raw_text0}**\n\ 📥EXTRACTED BY :{MR}'
+
+                
                                                                                                                                         
                 if "drive" in url:
                     try:
